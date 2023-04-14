6fffb51aa1b57af2f99cc62db70853c303efd6ce
krzys <krzysiekrudnik7@gmail.com>
2023-04-14 21:19:45 +0200

    c

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
 const canvas = document.querySelector('#canvas');
-class Point {
-    constructor(x, y, z) {
-      this.x = x;
-      this.y = y;
-      this.c = z;
-    }
-  }
+
 function drawSquare(ctx,x1,y1,x2,y2){
     drawLine(ctx, [x1, y1], [x2, y1]);
     drawLine(ctx, [x1, y1], [x1, y2]);
     drawLine(ctx, [x1, y2], [x2, y2]);
     drawLine(ctx, [x2, y2], [x2, y1]);
 }
-function drawCube(ctx,p1,p2,p3,p4,p5,p6,p7,p8){
-    drawLine(ctx, [p1.x, p1.y], [p2.x, p1.y]);
-    drawLine(ctx, [x1, y1], [x1, y2]);
-    drawLine(ctx, [x1, y2], [x2, y2]);
-    drawLine(ctx, [x2, y2], [x2, y1]);
-    
-    drawLine(ctx, [x1, y1], [x2, y1]);
-    drawLine(ctx, [x1, y1], [x1, y2]);
-    drawLine(ctx, [x1, y2], [x2, y2]);
-    drawLine(ctx, [x2, y2], [x2, y1]);
+function drawCube(ctx,p){
+  drawLine(ctx, [10000*p[0][0], 10000*p[0][1]], [10000*p[1][0], 10000*p[1][1]]);
+  console.log(p);
+  // console.log(p[01]);
+
 }
 if (canvas.getContext) {
     addEventListener("keydown", (event) => { });
@@ -29,7 +18,17 @@ if (canvas.getContext) {
     const ctx = canvas.getContext('2d');
     // drawLine(ctx, [0, 0], [500, 400]);
     // drawLine(ctx, [500, 0], [0, 400]);
-    drawSquare(ctx, 300, 400, 500, 200);
-    pmatrix=createPerspectiveMatrix(60,5/8,100,1000);
-    
+    // drawSquare(ctx, 300, 400, 500, 200);
+    cubeXYZ = [[1,1,1,1],[1,1,0.5,1],[1.5,1,0.5,1],[1.5,1,1,1],[1.5,1.5,1,1],[1.5,1.5,0.5,1],[1,1.5,0.5,1],[1,1.5,1,1]];
+    // drawCube(ctx, new Point(100,100,100),new Point(100,100,50),new Point(150,100,50),new Point(150,100,100),new Point(150,150,100),new Point(150,150,50),new Point(100,150,50),new Point(100,150,100));
+    pmatrix=createPerspectiveMatrix(60,5/8,120,1000);
+    cubeXYZ.forEach((e,index) => {
+      let res = multv(pmatrix,e);
+      res[0] = res[0] / res[2];
+      res[1] = res[1] / res[2];
+      res[2] = res[2] / res[2];
+      // console.log(res);
+      cubeXYZ[index] =res;
+    });
+    drawCube(ctx,cubeXYZ);
 }
--- js/mult.js
+++ js/mult.js
@@ -12,3 +12,17 @@ function mult(m1, m2) {
     }
     return result;
 }
+function multv(m1, m2) {
+    var result = [];
+    for (var i = 0; i < m1.length; i++) {
+        result[i] = [];
+
+        var sum = 0;
+        for (var k = 0; k < m1[0].length; k++) {
+            sum += m1[i][k] * m2[k];
+        }
+        result[i] = sum;
+
+    }
+    return result;
+}
